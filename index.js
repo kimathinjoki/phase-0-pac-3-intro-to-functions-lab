35436e30e3f8906f0d2a53b7d1a75fd63cd2128d
kimathinjoki <dfkimathinjoki@gmail.com>
2022-11-16 12:16:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,65 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+//console.log(shout("heeey"));
+
+
+function whisper(string2){
+    return  string2.toLowerCase();
+}
+
+//console.log(whisper("BEING THAT BASE"));
+
+
+function logShout(string){
+
+    console.log(string.toUpperCase());
+}
+
+logShout("hello")
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+
+
+function sayHiToHeadphonedRoommate(string){
+    /*const string = " Hello!";
+    console.log(string);
+    console.log(string.toLowerCase());
+    console.log(string);
+    console.log(string.toLowerCase());
+    console.log(string.toUpperCase());*/
+
+    if (string === string.toLowerCase()){
+        return "I can\'t hear you!";
+    }else if (string === string.toUpperCase()){
+        return "YES INDEED!"
+    }else (string === "Let\'s have dinner together!")
+        return "I would love to!"
+    
+}
+
+
+    /*console.log(string);
+    console.log(string.toUpperCase());
+    else if (string ===string.toUpperCase()){
+        console.log("YES INDEED!");
+    }
+    else if (string === "Let'shave dinner together"){
+        console.log("I would love to!");
+    }
+}*/
+
+
+
+
+
+
+
+        /*if (statement.toUpperCase() === string){
+            statement = "YES INDEED";
+            if (string === "I would love to"){
+                statement = "Let/'s have dinner together";*/
